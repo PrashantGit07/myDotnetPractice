4c22b0da0c7c8a6d65e458799754d130d8376528
prashant mishra <prashantmishra745885@gmail.com>
2024-02-15 12:15:10 +0530

    added folder where i practice dotnet with c#

---
+++ dotnetfirst/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//------------------------
+
+//ways to create a c# hello world program
+
+
+//simple example
+
+// class Program{
+//     static void Main(string[] args){
+//     Console.WriteLine("printing by method-1 simple example");
+//         System.Console.WriteLine("hello world");
+//     }
+// }
+
+
+//using system
+
+// using System;
+
+// class Program{
+//     static void Main(string[] args){
+//             Console.WriteLine("printing by method-2 using system");
+//         Console.WriteLine("hello world");
+//     }
+// }
+
+
+//using namespace
+
+// namespace ApplicationOne
+// {
+// // First class
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//                 Console.WriteLine("printing by method-3 using namespace");
+//             Console.WriteLine("Hello, world!");
+//         }
+//     }
+
+// // Second class
+//     public class AnotherClass
+//     {
+
+
+//         public void DisplayMessage()
+//         {
+//             Console.WriteLine("This is another class.");
+//         }
+//     }
+// }
+
+
+
+// using public modifier
+// using System;
+// public class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("pritning by method-4 using public modifier");
+//         Console.WriteLine("hello world");
+//     }
+// }
